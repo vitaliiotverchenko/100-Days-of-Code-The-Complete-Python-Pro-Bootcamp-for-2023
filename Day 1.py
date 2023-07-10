c735aa883615360e56109389041705ab47f5e8f7
Vitalii Otverchenko <43891562+vitaliiotverchenko@users.noreply.github.com>
2023-07-10 15:03:47 +0000

    Wrote a first programm from a course!

---
+++ Day 1.py
@@ -0,0 +1,4 @@
+print("Let's make a brand new band!")
+city = input("Where are you from? The name of your city? \n")
+pets_name = input("What is your pet's name? \n")
+print(f"Our band will have the name: {city} {pets_name}")

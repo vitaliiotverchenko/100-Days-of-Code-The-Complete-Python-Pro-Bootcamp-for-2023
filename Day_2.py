f50f2173e102dffed96044b26680a767e42da6f1
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-07-11 17:07:22 +0300

    changed rounding of a final amount

--- Day_2.py
+++ Day_2.py
@@ -1,7 +1,8 @@
 def tip():
     print('Welcome to the tip calculator.')
     bill = float(input('What was the total bill? $'))
-    people = int(input('How many people to split the bill? '))
     percent = int(input('What percentage tip would you like to geve? 10, 12, or 15? '))
-    return print(f'Each person should pay: ${round((bill+ bill * percent/100) / people, 1)}')
+    people = int(input('How many people to split the bill? '))
+    final_amount = "{:.2f}".format((bill + bill * (percent/100)) / people)
+    return print(f'Each person should pay: ${final_amount}')
 tip()

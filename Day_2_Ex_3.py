676fda918547ddbbed5145fc927e72a1ddfe1097
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-07-11 16:49:24 +0300

    I forgot to add a code into a file :)

---
+++ Day_2_Ex_3.py
@@ -0,0 +1,5 @@
+def years_left ():
+    years_left = 90 - int(input("What is your current age? "))
+    return print(f'You have {years_left * 365} days, {years_left * 52} weeks, and {years_left * 12} months left.')
+
+years_left()

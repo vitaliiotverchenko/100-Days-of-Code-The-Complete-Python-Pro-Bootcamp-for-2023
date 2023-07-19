9e93c1155be0954b3fd710b63a2c5eace8492122
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-07-19 23:05:53 +0300

    solved a simple issue

---
+++ Day_5_High_Score.py
@@ -0,0 +1,34 @@
+# Instructions
+# You are going to write a program that calculates the highest score from a List of scores.
+
+# e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+# 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
+# Important you are not allowed to use the max or min functions. The output words must match the example. i.e
+# 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
+
+# The highest score in the class is: x
+
+# Example Input
+# 78 65 89 86 55 91 64 89
+# In this case, student_scores would be a list that looks like: [78, 65, 89, 86, 55, 91, 64, 89]
+
+# Example Output
+# The highest score in the class is: 91
+
+
+
+student_scores = [int(i) for i in input("Input a list of student scores ").split()]
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
+
+
+
+
+

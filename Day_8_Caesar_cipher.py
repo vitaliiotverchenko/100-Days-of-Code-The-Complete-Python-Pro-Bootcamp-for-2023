608453e2fec832f649461d616d5403081480db31
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-07-26 22:50:21 +0300

    created a perfect Caesar cipher programm

--- Day_8_Caesar_cipher.py
+++ Day_8_Caesar_cipher.py
@@ -1,29 +1,41 @@
-# def caesar_cipher(text: str) -> str:
-#     # return ''.join([chr((ord(char) + key - 97) % 26 + 97) for char, key in zip(text, range(26))])
-import string
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            is_upper = char.isupper()
+            char = char.lower()
+            ascii_val = ord(char)
+            shifted_ascii = (ascii_val - ord('a') + shift) % 26 + ord('a')
+            if is_upper:
+                char = chr(shifted_ascii).upper()
+            else:
+                char = chr(shifted_ascii)
+        result += char
+    return result
 
-alphabet = frozenset(string.ascii_letters)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def main():
+    while True:
+        action = input("Do you want to encrypt or decrypt? (encrypt/decrypt): ").lower()
+        if action not in ('encrypt', 'decrypt'):
+            print("Invalid input. Please enter 'encrypt' or 'decrypt'.")
+            continue
 
-def encrypt(text, shift):
-    
+        text = input(f"Enter the text to {action}: ")
+        shift = int(input("Enter the shift value (a positive integer): "))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+        if action == 'encrypt':
+            encrypted_text = caesar_cipher(text, shift)
+            print("Encrypted text:", encrypted_text)
+        else:
+            decrypted_text = caesar_cipher(text, -shift)
+            print("Decrypted text:", decrypted_text)
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+        restart = input("Do you want to restart the program? (yes/no): ").lower()
+        if restart != "yes":
+            print("Goodbye! Exiting the program.")
+            break
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-
+if __name__ == "__main__":
+    main()

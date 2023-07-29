344ca0f55c882aa9984ac9e61f0ac2244aa12289
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-07-30 00:51:10 +0300

    deleted old versions of files

--- Day_1.py
+++
@@ -1,4 +0,0 @@
-print("Let's make a brand new band!")
-city = input("Where are you from? The name of your city? \n")
-pets_name = input("What is your pet's name? \n")
-print(f"Our band will have the name: {city} {pets_name}")

0fccb8a3da73ed8bef4e3cb1f1fbcb35463f5770
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-08-23 22:32:37 +0300

    started Day 19

---
+++ Day_19_Turtle_racing_and_turtle_drawing/main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+leonardo = Turtle()
+screen = Screen()
+
+
+
+
+
+def move_forward():
+    leonardo.forward(10)
+
+def move_backward():
+    leonardo.backward(10)
+
+def move_left():
+    leonardo.left(10)
+
+def move_right():
+    leonardo.right(10)
+
+def clear_screen():
+    leonardo.clear()
+    leonardo.penup()
+    leonardo.home()
+    leonardo.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear_screen)
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()

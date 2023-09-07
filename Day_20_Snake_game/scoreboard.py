d126f09f572c195631de76a4006edd40224bc2fb
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-09-07 22:01:55 +0300

    added ability to store the high score even after the game has closed

--- Day_20_Snake_game/scoreboard.py
+++ Day_20_Snake_game/scoreboard.py
@@ -1,13 +1,16 @@
 from turtle import Screen, Turtle
+
 ALIGMENT = "center"
 FONT = ("Courier", 24, "normal")
 TEXT_COLOR = "white"
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("C:\\Users\\38096\Desktop\\100DaysOfCodeChallenge\\100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2023\\Day_20_Snake_game\\data.txt", mode='r') as data:
+            self.high_score = int(data.read())
         self.color(f'{TEXT_COLOR}')
         self.penup()
         self.hideturtle()
@@ -16,11 +19,13 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align = ALIGMENT, font = FONT)
-                   
+        self.write(
+            f"Score: {self.score} High Score: {self.high_score}", align=ALIGMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
+        if self.score > self.high_score:
+            self.high_score = self.score
         self.update_score()
 
     # def game_over(self):
@@ -31,8 +36,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+        with open("C:\\Users\\38096\Desktop\\100DaysOfCodeChallenge\\100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2023\\Day_20_Snake_game\\data.txt", mode='w') as storage:
+            storage.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
-
-
-

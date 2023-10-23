47bd65097a8e728b87ceeda12136e9483b1f7685
Vitalii Otverchenko <vitaliiotverchenko@gmail.com>
2023-10-23 21:53:52 +0300

    finishe

--- Day_31_Duocards/main.py
+++ Day_31_Duocards/main.py
@@ -10,42 +10,52 @@ RIGHT_BUTTON_IMAGE = "Day_31_Duocards/images/right.png"
 WRONG_BUTTON_IMAGE = "Day_31_Duocards/images/wrong.png"
 CURRENT_LANGUAGE = "English"
 WORD = 'Start'
+# Time in miliseconds between flips of the card
+TIME_TO_WAIT = 5000
+current_card = {}
+to_learn = {}
+
 
 # ------------------ READ CSV DATA --------------
-data_frame = pd.read_csv("Day_31_Duocards/2000_words_en_ru.csv")
-dictionary = data_frame.to_dict(orient="records")
-# print(dictionary)
+# try to open words_to_learn.csv if it exists
+
+try:
+    original_data = pd.read_csv("Day_31_Duocards/words_to_learn.csv")
+except FileNotFoundError:
+    # if not, open 2000_words_en_ru.csv
+    original_data = pd.read_csv("Day_31_Duocards/2000_words_en_ru.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = original_data.to_dict(orient="records")
 
 # ----------------GENERATING RANDOM WORDS---------
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     canvas.itemconfig(canvas_image, image=card_front)
-    current_card = random.choice(dictionary)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(language_label, text="Russian", fill="black")
-    canvas.itemconfig(timer_label, fill="black")
-
     canvas.itemconfig(word_label, text=current_card
                       ["Russian"], fill="black")
+    flip_timer = window.after(5000, func=flip_card)
 
-    def english_word():
-        canvas.itemconfig(language_label, text="English", fill="white")
-        canvas.itemconfig(
-            word_label, text=current_card["English"], fill="white")
-        canvas.itemconfig(timer_label, fill="white")
 
-    # timer counting time from 5 to 0
-    def count_down(count):
-        canvas.itemconfig(timer_label, text=count)
-        if count > 0:
-            window.after(1000, count_down, count - 1)
-        else:
-            # change card image to the card_back.png
-            canvas.itemconfig(canvas_image, image=card_back)
-            english_word()
-    count_down(5)
-    # after 5 seconds flip the card
-    # window.after(5000, func=russian_translation)
+def is_known():
+    to_learn.remove(current_card)
+    pd.DataFrame(to_learn).to_csv(
+        "Day_31_Duocards/words_to_learn.csv", index=False)
+    next_card()
+
+
+def flip_card():
+    # global  flip_timer
+    # window.after_cancel(flip_timer)
+    canvas.itemconfig(canvas_image, image=card_back)
+    canvas.itemconfig(language_label, text="English", fill="white")
+    canvas.itemconfig(
+        word_label, text=current_card["English"], fill="white")
 
 
 # ----------------- UI SETUP ------------------
@@ -53,6 +63,9 @@ window = tk.Tk()
 window.title("Duocards")
 window.minsize(width=900, height=626)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+flip_timer = window.after(TIME_TO_WAIT, func=flip_card)
 
 canvas = tk.Canvas(width=800, height=526,
                    bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -63,15 +76,13 @@ language_label = canvas.create_text(
     400, 150, text='Language', font=("Arial", 40, "italic"))
 word_label = canvas.create_text(
     400, 263, text='Word', font=("Arial", 60, "bold"))
-timer_label = canvas.create_text(
-    400, 400, text='5', font=("Arial", 40, "italic"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 
-# ------------- BUTTONS -------------------
+# ------------- BUTTONS -------------------#
 known_button_image = tk.PhotoImage(file='Day_31_Duocards\images\\right.png')
 known_button = tk.Button(image=known_button_image,
-                         highlightthickness=0, borderwidth=0, command=next_card)
+                         highlightthickness=0, borderwidth=0, command=is_known)
 known_button.grid(column=0, row=1, sticky='n')
 
 
